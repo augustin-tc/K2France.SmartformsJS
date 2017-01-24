b1ae710e02d99c254f232ac8830b76888981a5ca
Augustin T. <augustin.tc@live.fr>
2017-01-24 18:16:37 +0100

    intial

---
+++ K2France.SmartformsJS/Scripts/serverUpdateControl.js
@@ -0,0 +1,20 @@
+﻿
+function serverUpdate(controlName) {
+    try {
+
+        var control = $("[name='" + controlName + "']");
+
+        var windowToUse = window;
+        var controlPath = 'Controllers/Controller/Controls/Control[@Name=\"' + controlName + '\"]';
+        var myControl = windowToUse.$sn(windowToUse.viewControllerDefinition, controlPath);
+        var controlInfoObj = new windowToUse.PopulateObject(null, control.html(), myControl.getAttribute('ID'));
+        windowToUse.executeControlFunction(myControl, 'SetValue', controlInfoObj);
+        windowToUse.raiseEvent(myControl.getAttribute('ID'), 'Control', 'OnChange');
+
+    }
+    catch (e) {
+
+        alert(e);
+
+    }
+}
